001e65406fe6ed324e1532cf8b2e2ae58af8a45e
edward-song-life-is-short <ybob566@gmail.com>
2021-06-20 11:28:16 -0400

    downloaded plotly library to graph stock market values

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Stock from './Stock'
 
--- src/Stock.js
+++ src/Stock.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import "./Stock.css"
+import Plot from 'react-plotly.js';
 
 class Stock extends React.Component {
     constructor(props) {
@@ -8,33 +10,69 @@ class Stock extends React.Component {
             stockChartYValues: []
         }
     }
-    
+
     componentDidMount() {
         this.fetchStock();
     }
 
     fetchStock() {
+        const pointThis = this;
         const API_KEY = 'EHM4W2PU9UBUEZZ2';
-        
+
+        console.log(pointThis);
+
+
+
         let StockSymbol = 'FB';
         let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`;
-    
+
+        let stockChartXValuesFunction = [];
+        let stockChartYValuesFunction = [];
+
         fetch(API_Call)
             .then(
-                function(response) {
+                function (response) {
                     return response.json();
                 }
             )
             .then(
-                function(data) {
+                function (data) {
                     console.log(data);
+
+                    for (var key in data['Time Series (Daily)']) {
+                        stockChartXValuesFunction.push(key);
+                        stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']);
+                    }
+
+                    pointThis.setState(
+                        {
+                            stockChartXValues: stockChartXValuesFunction,
+                            stockChartYValues: stockChartYValuesFunction
+                        }
+                    );
+                    //console.log(stockChartXValuesFunction);
                 }
             )
     }
 
     render() {
         return (
-            <div> Stock </div>
+            <div> Stock
+
+                <Plot
+                    data={[
+                        {
+                            x: this.state.stockChartXValues,
+                            y: this.state.stockChartYValues,
+                            type: 'scatter',
+                            mode: 'lines+markers',
+                            marker: { color: 'red' },
+                        },
+                    ]}
+                    layout={{ width: 1920, height: 1080, title: 'A Fancy Plot' }}
+                />
+
+            </div>
         )
     }
 }
